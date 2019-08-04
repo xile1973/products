cbe75a44b364fb90fb89210a689e5331db3d477c
Kelvin <xile1973@163.com>
2019-08-04 20:56:39 +0800

    add head

---
+++ products.py
@@ -0,0 +1,18 @@
+products = []
+while True:
+	name = input('请输入商品名称：')
+	if name == 'q':
+		break #跳出程序
+	price = input('请输入商品价格：')
+	price = int(price)
+	products.append([name,price])#把商品名称和价格装入products清单
+print(products)
+
+for p in products:   #利用for loop 把清单products中的元素读出来
+	print(p[0],'的价格是',p[1])
+
+with open('products.csv','w') as f:# 把清单products中的元素写到products.csv文件中
+	f.write('商品' + ',' + '价格' + '\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+		
